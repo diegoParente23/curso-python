a053f6e838ede25ad2167c2911e7da3b861a5a2d
Diego Parente <diego.parente@rddsolutions.com.br>
2019-03-12 22:51:21 -0300

    #4 *13-15 Variáveis e tipos de dados em python

---
+++ gguppe/tipo_numerico.py
@@ -0,0 +1,27 @@
+"""
+Tipo Numérico
+"""
+
+num = 1_000_000
+
+print(num)
+
+num /= 2
+
+print(num)
+
+num += 10
+
+print(num)
+
+num -= 2
+
+print(num)
+
+num *= 2
+
+print(num)
+
+num.__add__(20)
+
+print(num)
